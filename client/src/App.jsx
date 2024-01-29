d3b86571f7b1d27d2eefacd6be02f9f688ddc3b1
Shawn Nassabi <san7522@nyu.edu>
2024-01-29 18:23:30 +0400

    initial routes set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "./assets";
+import { Home, Create } from "./pages";
 
 const App = () => {
   return (
-    <>
-      <div className="flex justify-center items-center h-screen">
-        <div className="font-bold  text-3xl">Hello world!</div>
-      </div>
-    </>
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-slate-50 border-b border-b-slate-400 p-2">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-20" />
+        </Link>
+
+        <Link
+          to="/create"
+          className="bg-green-400 w-16 mx-8 h-fit text-center rounded-md text-sm"
+        >
+          Create
+        </Link>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./home";
+import Create from "./create";
+
+export { Home, Create };
