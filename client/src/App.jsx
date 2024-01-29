bbdb9a29e88e77fc25e1a64b0d9fd8eb6bc6a2c1
Shawn Nassabi <san7522@nyu.edu>
2024-01-29 17:48:15 +0400

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="font-bold  text-3xl">Hello world!</div>
+      </div>
+    </>
+  );
+};
+
+export default App;
